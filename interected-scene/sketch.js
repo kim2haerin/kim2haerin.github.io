3188aa1e05def0ee520f3af918a8b2dca9ea774c
kim2haerin <yedidiahh@gmail.com>
2024-09-27 11:03:20 -0600

    a chasing game

--- interected-scene/sketch.js
+++ interected-scene/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
 let playerX = 180;
 let playerY = 180;
 
@@ -27,30 +28,39 @@ let bHeight = 40;
 
 function setup() {
   createCanvas(400, 400);
+  background(135, 206, 235);
   side1=true;
   front1=true;
   
   side2=true;
   front2=true;
+  noStroke();
 }
 
 
 function draw() {
-  background(135, 206, 235);
-  text(mouseX, 200, 30);
-  text(mouseY, 260, 30); 
+  background(135, 206, 235); 
+  fill(225, 49, 35);
+  text("press any arrow", 200, 30);
+  fill(238, 141, 89 );
   
   //Ground
   noStroke();
   fill(0, 128, 0);
   rect(0, 200, width, height / 2);
+  
+  //frame
+  strokeWeight(25);
+  noFill();
+  stroke(51);
+  square(0, 0, 400);
+  noStroke();
 
 
   //character1
-  
   if(side1 === true){
   //side
-    fill(240, 230, 140);
+    fill(225, 49, 35);
     ellipse(playerX, playerY, 30, 30);
 
     fill(0, 255, 255);
@@ -65,7 +75,7 @@ function draw() {
   }
   else if(front1 ===true){
   //front
-    fill(240, 230, 140);
+    fill(225, 49, 35);
     ellipse(playerX, playerY, 30, 30);
 
     fill(0, 255, 255);
@@ -81,7 +91,7 @@ function draw() {
    
   if(side2 === true){
   //side
-    fill(140, 130, 140);
+    fill(238, 141, 89 );
     ellipse(playerX1, playerY1, 30, 30);
 
     fill(255, 0, 255);
@@ -92,11 +102,11 @@ function draw() {
     ellipse(playerX1, playerY1 + 30, 15, 15);
     fill(255);
     ellipse(playerX1, playerY1 + 30, 8, 8);
-
   }
+
   else if(front2 ===true){
   //front
-    fill(140, 130, 140);
+    fill(238, 141, 89);
     ellipse(playerX1, playerY1, 30, 30);
 
     fill(255, 0, 255);
@@ -106,52 +116,22 @@ function draw() {
     fill(0);
     ellipse(playerX1-14, playerY1 + 30, 8, 15);
     ellipse(playerX1+14, playerY1+ 30, 8, 15);
-  }
-  
-  function game(){
-    background(255);
-    stroke(51);
-    square(20, 20, 60);
-    noStroke();
-    fill(100, 0, 75);
-    rect(width/2, 450, width, 100); 
-  
-    // window frame
-    noFill();
-    stroke(0);
-    strokeWeight(15);
-    rect(width/2, height/2, width, height);
-  
-    //draw box
-    stroke(0);
-    strokeWeight(5);
-    fill(206, 100, 0);
-    rect(boxX, boxY, bWidth, bHeight);
-  }
-  
+  } 
   
   if(dist(playerX,playerY,playerX1,playerY1)<30){
   
     textSize(20);
     text("TAG",130,100);
   }
-  
-  function frame(){
-    noFill();
-    stroke(0);
-    strokeWeight(15);
-    //rect(width/2, height/2, width, height);
-    square(400, 400, 800);
-  } 
-  
+ 
 }
+
 
 function keyPressed() {
 
   if (keyCode === RIGHT_ARROW) {
     playerX += 10;
     side1=true;
-
   }
 
   if (keyCode === LEFT_ARROW) {
@@ -163,36 +143,34 @@ function keyPressed() {
     playerY -= 10;
     side1=false;
     front1=true;
-
   }
 
   if (keyCode === DOWN_ARROW) {
     playerY += 10;
     side1=false;
     front1=true;
-
   }
   
   //character2
-  //D
+  //press D
   if (keyCode === 68) {
     playerX1 += 10;
     side2=true;
 
   }
-  //A
+  //press A
   if (keyCode === 65) {
     playerX1 -= 10;
     side2=true;
   }
-  //w
+  //press w
   if (keyCode === 87) {
     playerY1 -= 10;
     side2=false;
     front2=true;
 
   }
-  //s
+  //press s
   if (keyCode === 83) {
     playerY1 += 10;
     side2=false;
