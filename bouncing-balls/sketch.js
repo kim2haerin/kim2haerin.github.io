be06e15a1fe89450b12bed569959d91a94821d62
Houngbo6600437 <6600437@spsd.sk.ca>
2024-10-04 10:15:24 -0600

    bouncing balls

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -2,43 +2,49 @@
 // yedidiah
 // oct 3, 2024
 let ballArray = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-let theBall = {
-  x: 200,
-  y: 300,
-  raduis: 50,
-  dx:3,
-  dy:2,
-};
-
-
 function draw() {
   background(220);
 
-  //move ball
-  theBall.x += theBall.dx;
-  theBall.y += theBall.dy;
-
-  //bounce if needed
-  if(theBall.x > width - theBall.raduis || theBall.x <0 + theBall.raduis){
-    theBall.dx *= -1;
+  for (let theBall of ballArray) {
+    //move ball
+    theBall.x += theBall.dx;
+    theBall.y += theBall.dy;
+  
+    //bounce if needed
+    if (theBall.x > width - theBall.radius || theBall.x < 0 + theBall.radius) {
+      theBall.dx *= -1;
+    }
+    if (theBall.y > height - theBall.radius || theBall.y < 0 + theBall.radius) {
+      theBall.dy *= -1;
+    }
+  
+    //show ball
+    noStroke();
+    fill(theBall.red, theBall.green, theBall.blue);
+    circle(theBall.x, theBall.y, theBall.radius*2);
   }
-  if(theBall.y > height - theBall.raduis || theBall.y <0 + theBall.raduis){
-    theBall.dy *=-1;
-  }
-  //show the ball
-  circle(theBall.x, theBall.y, theBall.raduis * 2);
 }
 
-function spawnBall(){
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function spawnBall(theX, theY) {
   let theBall = {
     x: theX,
     y: theY,
-    raduis: random(30, 60),
-    dx:random(-5, 5),
-    dy:random(-5, 5),
+    radius: random(30, 60),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    red: random(255),
+    green: random(255),
+    blue: random(255),
   };
+
+  ballArray.push(theBall);
 }
