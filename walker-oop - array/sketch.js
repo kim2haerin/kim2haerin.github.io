fc5e6f496a4d3c192fe450e1c91c2f2628168e0b
kim2haerin <yedidiahh@gmail.com>
2024-11-14 10:17:23 -0600

    OOP

--- walker-oop - array/sketch.js
+++ walker-oop - array/sketch.js
@@ -1,52 +1,58 @@
-// Walker OOP
+//walker OOP Demo
 
-class Walker{
-  constructor(x, y, theColor){
+class Walker {
+  constructor(x, y, theColor) {
     this.x = x;
     this.y = y;
-    this.speed = 10;
+    this.speed = 8;
+    this.radius = 3;
     this.color = theColor;
-    this.radius = 5;
   }
 
-  display(){
+  display() {
     noStroke();
     fill(this.color);
     circle(this.x, this.y, this.radius*2);
   }
-  move(){
+
+  move() {
     let choice = random(100);
-    if(choice < 25){
+    if (choice < 25) {
       //up
       this.y -= this.speed;
     }
-    else if (choice < 50){
+    else if (choice < 50) {
       //down
       this.y += this.speed;
     }
-    else if (choice < 75){
+    else if (choice < 75) {
       //left
       this.x -= this.speed;
     }
-    else{
-      this.x  += this.speed;
+    else {
+      //right
+      this.x += this.speed;
     }
   }
 }
 
-let winston;
-let radia;
+let walkerArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   winston = new Walker(width/2, height/2, "red");
-  winston = new Walker(200, 300, "blue");
+  walkerArray.push(winston);
 }
 
 function draw() {
-  winston.move();
-  winston.display();
+  for(let theWalker of walkerArray){
+    theWalker.move();
+    theWalker.display();
+  }
+}
 
-  radia.move();
-  radia.display();
+function mousePressed(){
+  let randomColor = color(random(255), random(255), random(255));
+  let someWalker = new Walker(mouseX, mouseY, randomColor);
+  walkerArray.push(someWalker);
 }
